6d61bb01812db3c66aa37b68ee3eaac39477ff30
Emmy2721 <96807367+Emmy2721@users.noreply.github.com>
2022-01-31 10:18:17 -0800

    commitNew

--- tech/urls.py
+++ tech/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    
+
 ]
+
--- tech/views.py
+++ tech/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from.models import product, TechType, Review
 # Create your views here.
 def index(request):
     return render(request, 'tech/index.html')
+    
+def products(request):
+    products_list=product.objects.all()
+    return render(request, 'tech/products.html',{'product_list: product_list'})
+         
